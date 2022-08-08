7d63187a08a406610d5980eb5f33d296c3f7cbae
YohansSamuel <yohanssamuel2014@gmail.com>
2022-08-08 17:29:42 +0300

    add fix into find_hashtags function

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -115,7 +115,11 @@ class TweetDfExtractor:
 
     # 
     def find_hashtags(self)->list:
-        hashtags =
+        hashtags = []
+        for tw in self.tweets_list:
+            hashtags.append(", ".join([hashtag_item['text'] for hashtag_item in tw['entities']['hashtags']]))
+            
+        return hashtags
 
     # 
     def find_mentions(self)->list:
