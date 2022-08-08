40a432bd8cb09e2e388511b8ce7a7c98fd86dd48
YohansSamuel <yohanssamuel2014@gmail.com>
2022-08-08 18:38:52 +0300

    fix convert_to_datetime function

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -3,10 +3,12 @@ class Clean_Tweets:
     """
     The PEP8 Standard AMAZING!!!
     """
+    #
     def __init__(self, df:pd.DataFrame):
         self.df = df
         print('Automation in Action...!!!')
-        
+    
+    #
     def drop_unwanted_column(self, df:pd.DataFrame)->pd.DataFrame:
         """
         remove rows that has column names. This error originated from
@@ -27,19 +29,17 @@ class Clean_Tweets:
         df = self.df.drop_duplicates(subset ='original_text')
         
         return df
-        
+
     # 
     def convert_to_datetime(self, df:pd.DataFrame)->pd.DataFrame:
         """
         convert column to datetime
         """
-        ----
+        self.df['created_at'] = pd.to_datetime(self.df['created_at'], errors='coerce')
         
-        ----
+        self.df = self.df[self.df['created_at'] >= '2020-12-31' ]
         
-        df = df[df['created_at'] >= '2020-12-31' ]
-        
-        return df
+        return self.df
     
     def convert_to_numbers(self, df:pd.DataFrame)->pd.DataFrame:
         """
