d42a1b27e9e09720d3917a2697f250adbe54ac25
YohansSamuel <yohanssamuel2014@gmail.com>
2022-08-09 14:16:59 +0300

    add unit test for find_is_sensitive

--- tests/test_extract_dataframe.py
+++ tests/test_extract_dataframe.py
@@ -77,7 +77,7 @@ class TestTweetDfExtractor(unittest.TestCase):
         self.assertEqual(self.df.find_friends_count(), friends_count)
 
     def test_find_is_sensitive(self):
-        self.assertEqual(self.df.is_sensitive(), <provide a list of the first five is_sensitive values>)
+        self.assertEqual(self.df.is_sensitive(), [None, None, None, None, None])
 
 
     # def test_find_hashtags(self):
