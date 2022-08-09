f16e13cde179619ce21358ff3ee9cc62cd1cc674
YohansSamuel <yohanssamuel2014@gmail.com>
2022-08-09 16:16:02 +0300

    change test case of find_full_text

--- tests/test_extract_dataframe.py
+++ tests/test_extract_dataframe.py
@@ -75,8 +75,8 @@ class TestTweetDfExtractor(unittest.TestCase):
         ]
         self.assertEqual(self.df.find_full_text(), text)
 
-    # def test_find_sentiments(self):
-    #     self.assertEqual(self.df.find_sentiments(self.df.find_full_text()), ([-0.125, -0.1, 0.0, 0.1, -6.938893903907228e-18]))
+    def test_find_sentiments(self):
+        self.assertEqual(self.df.find_sentiments(self.df.find_full_text()), ([0.190625, 0.1, 0.0, 0.35, 0.55625]))
 
     def test_find_screen_name(self):
         name = ['i_ameztoy', 'ZIisq', 'Fin21Free', 'Fin21Free', 'VizziniDolores']
