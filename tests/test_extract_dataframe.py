5105090b8e5a70b53ecd68d5de4862d32488326c
YohansSamuel <yohanssamuel2014@gmail.com>
2022-08-09 14:13:14 +0300

    add unit test for find_screen_name

--- tests/test_extract_dataframe.py
+++ tests/test_extract_dataframe.py
@@ -65,7 +65,7 @@ class TestTweetDfExtractor(unittest.TestCase):
         self.assertEqual(self.df.find_sentiments(self.df.find_full_text()), ([0.16666666666666666, 0.13333333333333333, 0.3166666666666667, 0.08611111111111111, 0.27999999999999997], [0.18888888888888888, 0.45555555555555555, 0.48333333333333334, 0.19722222222222224, 0.6199999999999999]))
 
     def test_find_screen_name(self):
-        name = <provide a list of the first five screen names>
+        name = ['ketuesriche', 'Grid1949', 'LeeTomlinson8', 'RIPNY08', 'pash22']
         self.assertEqual(self.df.find_screen_name(), name)
 
     def test_find_followers_count(self):
