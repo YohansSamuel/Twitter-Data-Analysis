8a2eebf051e183d4aed3982fe723e3c9f92ada58
YohansSamuel <yohanssamuel2014@gmail.com>
2022-08-09 14:15:33 +0300

    add unit test for find_friends_count

--- tests/test_extract_dataframe.py
+++ tests/test_extract_dataframe.py
@@ -73,7 +73,7 @@ class TestTweetDfExtractor(unittest.TestCase):
         self.assertEqual(self.df.find_followers_count(), f_count)
 
     def test_find_friends_count(self):
-        friends_count = <provide a list of the first five friend's counts>
+        friends_count = [351, 92, 1176, 2704, 30819]
         self.assertEqual(self.df.find_friends_count(), friends_count)
 
     def test_find_is_sensitive(self):
