2b6bb8847db3f982ccb232a3aeb101c05d501b54
YohansSamuel <yohanssamuel2014@gmail.com>
2022-08-12 05:34:10 +0300

    add database connection

--- tests/add_data_into_database.py
+++ tests/add_data_into_database.py
@@ -2,3 +2,18 @@ import os
 import pandas as pd
 import mysql.connector as mysql
 from mysql.connector import Error
+
+def DBConnect(dbName=None):
+    """
+    Parameters
+    ----------
+    dbName :
+        Default value = None)
+    Returns
+    -------
+    #os.getenv('mysqlPass')
+    """
+    conn = mysql.connect(host='localhost', user='root', password='root',
+                         database=dbName, buffered=True)
+    cur = conn.cursor()
+    return conn, cur
