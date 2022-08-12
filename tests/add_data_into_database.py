f08e060c9c5990da0d7f96bda17bbc00760f3947
YohansSamuel <yohanssamuel2014@gmail.com>
2022-08-12 05:36:39 +0300

    create a database

--- tests/add_data_into_database.py
+++ tests/add_data_into_database.py
@@ -13,7 +13,30 @@ def DBConnect(dbName=None):
     -------
     #os.getenv('mysqlPass')
     """
-    conn = mysql.connect(host='localhost', user='root', password='root',
+    conn = mysql.connect(host='localhost', user='root', password='********',
                          database=dbName, buffered=True)
     cur = conn.cursor()
     return conn, cur
+
+def emojiDB(dbName: str) -> None:
+    conn, cur = DBConnect(dbName)
+    dbQuery = f"ALTER DATABASE {dbName} CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;"
+    cur.execute(dbQuery)
+    conn.commit()
+
+def createDB(dbName: str) -> None:
+    """
+    Parameters
+    ----------
+    dbName :
+        str:
+    dbName :
+        str:
+    dbName:str :
+    Returns
+    -------
+    """
+    conn, cur = DBConnect()
+    cur.execute(f"CREATE DATABASE IF NOT EXISTS {dbName};")
+    conn.commit()
+    cur.close()
