1be8f6503cb2d9d0f15e929420692533ace02375
YohansSamuel <yohanssamuel2014@gmail.com>
2022-08-12 04:53:31 +0300

    add test class for cleant_tweets_dataframe

--- tests/test_clean_tweets_dataframe.py
+++ tests/test_clean_tweets_dataframe.py
@@ -16,3 +16,13 @@ from extract_dataframe import TweetDfExtractor
 # Provide the path to the samples tweets file you created below
 sampletweetsjsonfile = "./tests/sampletweets.json"   #put here the path to where you placed the file e.g. ./sampletweets.json. 
 _, tweet_list = read_json(sampletweetsjsonfile)
+
+class TestCleanTweetsDataFrame(unittest.TestCase):
+    """
+		A class for unit-testing function in the fix_clean_tweets_dataframe.py file
+
+		Args:
+        -----
+			unittest.TestCase this allows the new class to inherit
+			from the unittest module
+	"""
