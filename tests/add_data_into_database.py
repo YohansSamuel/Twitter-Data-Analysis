f645786b965ce793913de0b36e0762f67246879a
YohansSamuel <yohanssamuel2014@gmail.com>
2022-08-12 05:56:31 +0300

    execute the fetch

--- tests/add_data_into_database.py
+++ tests/add_data_into_database.py
@@ -144,3 +144,46 @@ def insert_to_tweet_table(dbName: str, df: pd.DataFrame, table_name: str) -> Non
             conn.rollback()
             print("Error: ", e)
     return
+
+# execute the fetch
+def db_execute_fetch(*args, many=False, tablename='', rdf=True, **kwargs) -> pd.DataFrame:
+    """
+    Parameters
+    ----------
+    *args :
+    many :
+         (Default value = False)
+    tablename :
+         (Default value = '')
+    rdf :
+         (Default value = True)
+    **kwargs :
+    Returns
+    -------
+    """
+    connection, cursor1 = DBConnect(**kwargs)
+    if many:
+        cursor1.executemany(*args)
+    else:
+        cursor1.execute(*args)
+
+    # get column names
+    field_names = [i[0] for i in cursor1.description]
+
+    # get column values
+    res = cursor1.fetchall()
+
+    # get row count and show info
+    nrow = cursor1.rowcount
+    if tablename:
+        print(f"{nrow} records fetched from {tablename} table")
+
+    cursor1.close()
+    connection.close()
+
+    # return result
+    if rdf:
+        return pd.DataFrame(res, columns=field_names)
+    else:
+        return res
+
